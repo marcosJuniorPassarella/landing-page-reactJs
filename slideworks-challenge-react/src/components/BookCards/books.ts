e0eb5bbd955037efbdff121dd8d49ebd788e6a0c
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-03-25 09:43:46 -0300

    main - Althered images url

--- slideworks-challenge-react/src/components/BookCards/books.ts
+++ slideworks-challenge-react/src/components/BookCards/books.ts
@@ -19,7 +19,7 @@ const books: BooksDatas[] = [
     description:
       "She did it at all,' said the Hatter replied. 'Of course you know that you're mad?' 'To begin with,' said the Caterpillar. Alice thought she might as well as she was beginning to feel a little before.",
     isbn: "9785733314648",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1990-05-09",
     publisher: "In Omnis",
   },
@@ -31,7 +31,7 @@ const books: BooksDatas[] = [
     description:
       "Rabbit came near her, about four inches deep and reaching half down the chimney?--Nay, I shan't! YOU do it!--That I won't, then!--Bill's to go down the chimney close above her: then, saying to.",
     isbn: "9787570046072",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1985-07-01",
     publisher: "Occaecati In",
   },
@@ -43,7 +43,7 @@ const books: BooksDatas[] = [
     description:
       "I will tell you my history, and you'll understand why it is you hate--C and D,' she added in an encouraging opening for a moment that it ought to eat or drink something or other; but the Gryphon.",
     isbn: "9781499801705",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1974-08-24",
     publisher: "Dolorum Voluptas",
   },
@@ -55,7 +55,7 @@ const books: BooksDatas[] = [
     description:
       "Duchess by this time, sat down again very sadly and quietly, and looked at it again: but he would not open any of them. However, on the whole party at once set to work very diligently to write out a.",
     isbn: "9794619893337",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1997-12-31",
     publisher: "Rem Beatae",
   },
@@ -67,7 +67,7 @@ const books: BooksDatas[] = [
     description:
       "She said the King, rubbing his hands; 'so now let the jury--' 'If any one left alive!' She was a good deal worse off than before, as the doubled-up soldiers were silent, and looked at poor Alice.",
     isbn: "9782168944242",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1972-07-18",
     publisher: "Iusto Temporibus",
   },
@@ -79,7 +79,7 @@ const books: BooksDatas[] = [
     description:
       "I should say what you like,' said the Duchess, as she could. 'The game's going on shrinking rapidly: she soon made out that she never knew so much about a whiting to a lobster--' (Alice began to.",
     isbn: "9789002425684",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1988-05-06",
     publisher: "Tenetur Tenetur",
   },
@@ -91,7 +91,7 @@ const books: BooksDatas[] = [
     description:
       "Lory, as soon as it can't possibly make me giddy.' And then, turning to Alice, very loudly and decidedly, and there she saw them, they set to partners--' '--change lobsters, and retire in same.",
     isbn: "9797878180323",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "2017-07-18",
     publisher: "Facilis Amet",
   },
@@ -103,7 +103,7 @@ const books: BooksDatas[] = [
     description:
       "March Hare. Alice sighed wearily. 'I think I could, if I might venture to say a word, but slowly followed her back to the shore. CHAPTER III. A Caucus-Race and a crash of broken glass, from which.",
     isbn: "9785444093092",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1991-08-10",
     publisher: "Ut Modi",
   },
@@ -115,7 +115,7 @@ const books: BooksDatas[] = [
     description:
       "It doesn't look like it?' he said. 'Fifteenth,' said the last time she saw in another moment it was out of it, and found herself at last it unfolded its arms, took the hookah out of breath, and till.",
     isbn: "9799041207813",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1989-03-05",
     publisher: "Eos Magni",
   },
@@ -127,7 +127,7 @@ const books: BooksDatas[] = [
     description:
       "Queen. First came ten soldiers carrying clubs; these were ornamented all over crumbs.' 'You're wrong about the same thing as a partner!' cried the Mouse, turning to Alice. 'Only a thimble,' said.",
     isbn: "9791741322582",
-    image: "http://placeimg.com/480/640/any",
+    image: "https://picsum.photos/480/640",
     published: "1987-10-02",
     publisher: "Nam Voluptatem",
   },
